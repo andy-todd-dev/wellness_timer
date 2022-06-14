4f4efbf12a6d5b5fd09b4a84e9647cc2bc1b1dbc
Andy Todd <100069128+andy-todd-dev@users.noreply.github.com>
2022-06-14 09:43:43 +0000

    Add controls and sound

--- wellness/src/meditationTimer/MeditationTimer.js
+++ wellness/src/meditationTimer/MeditationTimer.js
@@ -1,18 +1,28 @@
 import React from "react";
 import { useTimer } from "use-timer";
 import TimerDisplay from "./TimerDisplay";
+import useSound from "use-sound";
+
+import timerFinishedSfx from "../sounds/singingxbowl.wav";
 
 const MeditationTimer = () => {
-  const { time } = useTimer({
-    initialTime: 20 * 60,
+  const [play] = useSound(timerFinishedSfx);
+
+  const { time, start, pause, reset, status } = useTimer({
+    initialTime: 10, // 20 * 60,
     timerType: "DECREMENTAL",
-    autostart: true,
     endTime: 0,
+    onTimeOver: play,
   });
 
   return (
     <div className="meditation-timer">
       <TimerDisplay duration={time} />
+      {status !== "RUNNING" && <button onClick={start}>Start</button>}
+      {status === "RUNNING" && <button onClick={pause}>Pause</button>}
+      {(status === "PAUSED" || time === 0) && (
+        <button onClick={reset}>Reset</button>
+      )}
     </div>
   );
 };
