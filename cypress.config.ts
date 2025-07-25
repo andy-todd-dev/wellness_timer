2418b752cb3836dc60118dbc016bd74fb643e44c
Andrew Todd <emailandytodd@gmail.com>
2025-07-25 09:49:44 +0000

    Add basic BDD cypress test

---
+++ cypress.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "cypress";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
+
+export default defineConfig({
+  e2e: {
+    specPattern: "**/*.feature",
+    baseUrl: "http://localhost:3000",
+    supportFile: false,
+    async setupNodeEvents(on, config) {
+      // Register the Cucumber preprocessor as the file preprocessor
+      await addCucumberPreprocessorPlugin(on, config);
+      on("file:preprocessor", createBundler({
+        plugins: [
+          createEsbuildPlugin(config)
+        ],
+      }));
+      return config;
+    },
+  },
+});
---
+++ tests/basic.feature
@@ -0,0 +1,5 @@
+Feature: Wellness timer
+    Scenario: Loading the wellness timer
+        Given I have a wellness timer
+        When I load the wellness timer
+        Then I should see the wellness timer loaded
