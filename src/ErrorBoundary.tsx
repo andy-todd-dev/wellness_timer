862d3251d7f2ad6f924e9ff87c1caa24fe762fda
Andrew Todd <emailandytodd@gmail.com>
2025-07-30 15:51:58 +0000

    Add ErrorBoundary component and update index to use TypeScript

---
+++ src/ErrorBoundary.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+interface ErrorBoundaryProps {
+    children: React.ReactNode;
+}
+
+interface ErrorBoundaryState {
+    hasError: boolean;
+}
+class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+
+    constructor(props: ErrorBoundaryProps) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+        return { hasError: true };
+    }
+
+    componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+        // You can also log the error to an error reporting service
+        console.error("Error caught in ErrorBoundary:", error, errorInfo);
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <h1>Whoops! Something went wrong :/</h1>;
+        }
+        return this.props.children; 
+    }
+}
+export default ErrorBoundary;
--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,18 @@ import "./index.css";
 import App from "./App";
 import { CssBaseline } from "@mui/material";
 import { ThemeConfigurer } from "./ThemeConfigurer";
+import ErrorBoundary from "./ErrorBoundary";
 
 const container = document.getElementById("root");
 if (!container) throw new Error('Root container missing in index.html');
 const root = ReactDOM.createRoot(container);
 root.render(
   <React.StrictMode>
-    <ThemeConfigurer>
-      <CssBaseline />
-      <App />
-    </ThemeConfigurer>
+    <ErrorBoundary>
+      <ThemeConfigurer>
+        <CssBaseline />
+        <App />
+      </ThemeConfigurer>
+    </ErrorBoundary>
   </React.StrictMode>
 );
