9df114fc45a65e79b4956dc519049668f5a682b9
Andrew Todd <emailandytodd@gmail.com>
2026-03-25 13:56:54 +0000

    Update dependency major versions

--- cypress.config.ts
+++ cypress.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
         "file:preprocessor",
         createBundler({
           plugins: [createEsbuildPlugin(config)],
-        })
+        }),
       );
       return config;
     },
